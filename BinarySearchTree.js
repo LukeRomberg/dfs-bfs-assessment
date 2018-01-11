33b99560874750d1b6210ee6f8d3f8cdceb3183e
Luke Romberg <Lukerromberg@gmail.com>
2018-01-11 08:42:02 -0700

    Finished this shiiiiiz

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -35,11 +35,30 @@ class BinarySearchTree {
 	dfs(cb, node=this.root) {
     // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
 		// Expected order: '25, 15, 17, 197, 97, 9997'
+    cb(node.data)
+    if(node.left){
+      this.dfs(cb, node.left)
+    }
+    if(node.right){
+      this.dfs(cb, node.right)
+    }
 	}
 
 	bfs(cb, node=this.root) {
     // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
     // Expected order: 25, 15, 197, 17, 97, 9997
+    var nodes = []
+    nodes.push(node)
+    while(nodes.length>0){
+      let current = nodes.shift()
+      cb(current.data)
+      if(current.left){
+        nodes.push(current.left)
+      }
+      if(current.right){
+        nodes.push(current.right)
+      }
+    }
 	}
 }
 
